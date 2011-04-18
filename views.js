5c3d942fd1e8db1d114a5c990dfa3a1b65b8e9eb
dolb <dolb1987@gmail.com>
2011-04-18 21:11:48 +0200

    widok preparujacy dokument ze statyczna mapka googli z lokalizacja ufo + ikona

---
+++ views.js
@@ -0,0 +1,24 @@
+var couchapp = require('couchapp');
+ddoc = {
+    _id: '_design/stats'
+  , views: {}
+}
+module.exports = ddoc;
+
+ddoc.views.mapaGoogli = 
+{
+  map: function(doc) 
+	{
+  	if((doc.location != "" && doc.sighted_at != "0000" && doc.duration != "")) 
+		{
+    	var mapka = "http://maps.google.com/maps/api/staticmap?size=480x480&sensor=true&markers=icon:http://img21.imageshack.us/img21/5336/ufou.png|"+doc.location;
+    	var tekst = doc.duration + " spotkanie z ufo";
+    	emit({tytul: tekst, czas: doc.sighted_at, icon: "ufo", opis: doc.description, image: mapka}, null);
+  	}
+	},
+
+  reduce: function(keys, values) 
+	{
+    return null;
+  }
+}
